2ffe17e900dd78e63bd02856d0c6f8cabbd58e4f
brthngnsh <demongod250299@gmail.com>
2020-09-29 02:11:35 +0530

    Updates

---
+++ precache-manifest.7dffd04f5af537522f1da0d9387aae69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40366e01aedf6d6891cff8002017ecab",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "ee32e1604373d6471f80",
+    "url": "/Covid-Tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "384a618dbc547afa0e7f",
+    "url": "/Covid-Tracker/static/css/main.bc9cc737.chunk.css"
+  },
+  {
+    "revision": "ee32e1604373d6471f80",
+    "url": "/Covid-Tracker/static/js/2.a19ac823.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/Covid-Tracker/static/js/2.a19ac823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384a618dbc547afa0e7f",
+    "url": "/Covid-Tracker/static/js/main.b135d771.chunk.js"
+  },
+  {
+    "revision": "b619aef232c750e36b8d",
+    "url": "/Covid-Tracker/static/js/runtime-main.fe252f1e.js"
+  }
+]);
